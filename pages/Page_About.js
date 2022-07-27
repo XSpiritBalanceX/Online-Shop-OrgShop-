c758cdcc4f144edeaea254a44a7f323c775c903f
Storm <lenagreidon@mail.ru>
2022-07-27 18:02:15 +0300

    process

--- pages/Page_About.js
+++ pages/Page_About.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './Page_About.css'
 
@@ -14,17 +14,32 @@ export default props=>{
     },
     [count]);
 
+    //проверка правильности emil
     const [emailCurrValue, setEmail] = useState('');
-    const [numberCurrValue, setNumber] = useState('');
-    const [agreeWiPo, setCheck] = useState(false);
     const [forEmail, setForEmail] = useState('');
+    //проверка правильности номера телефона
+    const [numberCurrValue, setNumber] = useState('');
+    const [forNumber, setForNumber] = useState('');
+    //проверка согласия с пользовательским соглашением
+    const [agreeWiPo, setCheck] = useState(false);
+    const [forPoliti, setForPolitic] = useState('');
+    
+    //валидируем поля подписки на новости
   function handleClick() {
     let regForEmail=/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/;
-    if(!regForEmail.test(emailCurrValue.toLowerCase())){
-      setForEmail('Введите корректный Email');
-    }
+    let regForNumber=/^\s*\+?375((33\d{7})|(29\d{7})|(44\d{7}|)|(25\d{7}))\s*$/;
+    let errorEmail;
+    let errorNumber;
+    let errorCheck;
+    !regForEmail.test(emailCurrValue.toLowerCase())?errorEmail='Введите корректный Email':errorEmail='';
+      setForEmail(errorEmail);
     
-    console.log(forEmail)
+    !regForNumber.test(numberCurrValue)?errorNumber='Ошибка. Формат номера +375 29/44/33 XXXXXXX':
+    errorNumber='';      
+    setForNumber(errorNumber);
+      
+    !agreeWiPo?errorCheck='Вы должны дать согласие на обработку персональных данных':errorCheck='';
+      setForPolitic(errorCheck);
   }
 
   return (
@@ -61,19 +76,19 @@ export default props=>{
     </div>
     <div className='inputAll'>
     <div className="text-field text-field_floating-2">      
-    <input className="text-field__input"  type="text"  name="email" placeholder="Введите Email" value={emailCurrValue} onChange={()=>setEmail(event.target.value)}/>
+    <input className="text-field__input"  type="text"  name="email" placeholder="Введите Email" value={emailCurrValue} onChange={()=>setEmail(event.target.value)} onFocus={()=>setForEmail('')}/>
     <label className="text-field__label" htmlFor="email" >Email</label>
-    <span className='error'>{forEmail}</span>
+    <p className='error'>{forEmail}</p>
     </div>
     <div className="text-field text-field_floating-2">
-    <input className="text-field__input" type="text"  name="number" placeholder="Введите номер телефона" value={numberCurrValue} onChange={()=>setNumber(event.target.value)}/>
+    <input className="text-field__input" type="text"  name="number" placeholder="Введите номер телефона" value={numberCurrValue} onChange={()=>setNumber(event.target.value)} onFocus={()=>setForNumber('')}/>
     <label className="text-field__label" htmlFor="number">Номер телефона</label>
-    <span className='error'>{forEmail}</span>
+    <p className='error'>{forNumber}</p>
     </div>
     <div className='checkForRule'>
-      <input type='checkbox' name='agreeForRule' checked={agreeWiPo} onChange={()=>setCheck(event.target.value)}/>
+      <input type='checkbox' name='agreeForRule' checked={agreeWiPo} onChange={()=>setCheck(event.target.checked)}/>
       <label className="agrFoRu" htmlFor="agreeForRule">Я даю согласие на обработку персональных данных</label>
-      <span className='error'>{forEmail}</span>
+      <p className='error'>{forPoliti}</p>
     </div>
     </div>
     <input type={'button'} name='podpiska' defaultValue={'Подписаться'} className='buttonPodp' onClick={handleClick}/>
@@ -82,63 +97,3 @@ export default props=>{
   );
   
 }
-
-/* class Page_About extends React.PureComponent {
-   
-  render() {
-    
-    return (
-      <React.Fragment>
-        <div className='aboutShop'>
-        <h3>О МАГАЗИНЕ</h3>
-      </div>
-      <div className='firstContent'>
-        <div className='firstImg'></div>
-        <div className='firstInfo'><p>Наш магазин не только продает безопасную органическую косметику, 
-          мы внимательны к каждому нашему клиенту, также мы хотим продлить красоту 
-          и молодость Вашей кожи.  </p>
-          <div className='firstStat'>
-          <div><p className='countInfo' >10</p><p>Магазинов</p></div>
-          <div><p className='countInfo'>100+</p><p>Заказов в день</p></div>
-          <div><p className='countInfo'>25</p><p>Сотрудников</p></div>
-          </div>
-          </div>
-      </div>
-      <div className='secondContent'><div className='firstInfo'><p>Магазин был основан в 2021 году и 
-        с тех пор мы постоянно растем, чтобы каждая женщина могла  радовать себя 
-        органической косметикой. Наши консультанты постоянно проходят обучение, 
-        чтобы ты могла получить профессиональный совет в уходе за своей кожей.   </p>
-          <div className='firstStat secStat'>
-          <div><p className='countInfo'>20</p><p>Брендов</p></div>
-          <div><p className='countInfo'>1000+</p><p>Улыбок женщин</p></div>
-          </div>
-          </div>
-          <div className='secImg'></div>
-          <div className='thirImg'></div></div>
-          <div className='podpiska'>
-          <div className='aboutPodp '>
-        <h3>ОСТАВАЙСЯ В КУСРЕ ПОСЛЕДНИХ НОВОСТЕЙ</h3>
-      </div>
-      <div className='inputAll'>
-      <div className="text-field text-field_floating-2">
-      <input className="text-field__input" type="email"  name="email" placeholder="alexander@itchief.ru"/>
-      <label className="text-field__label" htmlFor="email">Email</label>
-      </div>
-      <div className="text-field text-field_floating-2">
-      <input className="text-field__input" type="email"  name="number" placeholder="alexander@itchief.ru"/>
-      <label className="text-field__label" htmlFor="number">Номер телефона</label>
-      </div>
-      <div className='checkForRule'>
-        <input type='checkbox' name='agreeForRule'/>
-        <label className="agrFoRu" htmlFor="agreeForRule">Я даю согласие на обработку персональных данных</label>
-      </div>
-      </div>
-      </div>
-      </React.Fragment>
-    );
-    
-  }
-
-}
-    
-export default Page_About; */
